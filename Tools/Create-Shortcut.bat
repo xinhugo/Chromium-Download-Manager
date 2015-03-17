858520b7a5e26db1795384e146531f1e20fb85dd
xinhugo <hugox.chan@gmail.com>
2015-03-17 15:07:04 +0800

    Fix bugs

--- Tools/Create-Shortcut.bat
+++ Tools/Create-Shortcut.bat
@@ -3,4 +3,3 @@ cd /d "%~dp0"
 cd..
 Tools\shortcut.exe /A:C /F:"Chromium.lnk" /T:"%cd%\chrome-win32\chrome.exe" /P:"--no-first-run --User-data-dir="%cd%\Data" --disk-cache-dir="%USERPROFILE%\ChromeCache""" /W:"%cd%\chrome-win32"
 if exist pepflashplayer.dll Tools\shortcut.exe /A:C /F:"Chromium-PPAPI-FLASH.lnk" /T:"%cd%\chrome-win32\chrome.exe" /P:"--no-first-run --User-data-dir="%cd%\Data" --disk-cache-dir="%USERPROFILE%\ChromeCache" --ppapi-flash-path=%cd%\pepflashplayer.dll --ppapi-flash-version=17.0.0.134"" /W:"%cd%\chrome-win32"
-pause
