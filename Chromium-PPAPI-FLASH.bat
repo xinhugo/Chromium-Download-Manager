951ecad06485cd1019b81d581e6ff465210d7960
xinhugo <hugox.chan@gmail.com>
2015-03-17 17:44:07 +0800

    Remove duplicate files

--- Chromium-PPAPI-FLASH.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-if not exist chrome-win32\chrome.exe  echo.&echo.&echo.&echo.&echo.&echo.&echo                          未发现 chrome-win32\chrome.exe，按任意键退出。&echo.&echo.&echo.&echo.&echo                                         按任意键返回&pause>nul& exit
-start %~dp0chrome-win32\chrome.exe --no-first-run --User-data-dir="%~dp0Data" --disk-cache-dir="%USERPROFILE%\ChromeCache" --ppapi-flash-path="%~dp0pepflashplayer.dll" --ppapi-flash-version=17.0.0.134
