781b0fee246ccdfbd46519f556b23c5ba72acd20
xinhugo <hugox.chan@gmail.com>
2015-03-17 15:04:46 +0800

    Fix bugs

--- Tools/Create-Shortcut.bat
+++ Tools/Create-Shortcut.bat
@@ -1,4 +1,6 @@
 @echo off
+cd /d "%~dp0"
 cd..
-Tools\shortcut.exe /A:C /F:"Chromium.lnk" /T:"%~dp0chrome-win32\chrome.exe" /P:"--no-first-run --User-data-dir="%~dp0Data" --disk-cache-dir="%USERPROFILE%\ChromeCache""" /W:"%~dp0chrome-win32"
-if exist pepflashplayer.dll Tools\shortcut.exe /A:C /F:"Chromium-PPAPI-FLASH.lnk" /T:"%~dp0chrome-win32\chrome.exe" /P:"--no-first-run --User-data-dir="%~dp0Data" --disk-cache-dir="%USERPROFILE%\ChromeCache" --ppapi-flash-path=%~dp0pepflashplayer.dll --ppapi-flash-version=17.0.0.134"" /W:"%~dp0chrome-win32"
+Tools\shortcut.exe /A:C /F:"Chromium.lnk" /T:"%cd%\chrome-win32\chrome.exe" /P:"--no-first-run --User-data-dir="%cd%\Data" --disk-cache-dir="%USERPROFILE%\ChromeCache""" /W:"%cd%\chrome-win32"
+if exist pepflashplayer.dll Tools\shortcut.exe /A:C /F:"Chromium-PPAPI-FLASH.lnk" /T:"%cd%\chrome-win32\chrome.exe" /P:"--no-first-run --User-data-dir="%cd%\Data" --disk-cache-dir="%USERPROFILE%\ChromeCache" --ppapi-flash-path=%cd%\pepflashplayer.dll --ppapi-flash-version=17.0.0.134"" /W:"%cd%\chrome-win32"
+pause
