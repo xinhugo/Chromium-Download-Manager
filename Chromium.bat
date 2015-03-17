b4bce70044e6598366cb0c8ebf8115dd284af476
xinhugo <hugox.chan@gmail.com>
2015-03-17 17:41:58 +0800

    Fix bugs

--- Chromium.bat
+++ Chromium.bat
@@ -1,2 +1,3 @@
 @echo off
-"%~dp0chrome-win32\chrome.exe" --no-first-run --User-data-dir="%~dp0Data" --disk-cache-dir="%USERPROFILE%\ChromeCache"
+if not exist chrome-win32\chrome.exe  echo.&echo.&echo.&echo.&echo.&echo.&echo                          未发现 chrome-win32\chrome.exe，按任意键退出。&echo.&echo.&echo.&echo.&echo                                         按任意键返回&pause>nul& exit
+start %~dp0chrome-win32\chrome.exe --no-first-run --User-data-dir="%~dp0Data" --disk-cache-dir="%USERPROFILE%\ChromeCache"
