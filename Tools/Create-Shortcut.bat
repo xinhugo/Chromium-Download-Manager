6522b186da9b61290853745d83c7aeba5977e2e0
xinhugo <hugox.chan@gmail.com>
2015-03-21 00:22:55 +0800

    Update Create-Shortcut

--- Tools/Create-Shortcut.bat
+++ Tools/Create-Shortcut.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
 cd..
-Tools\shortcut.exe /A:C /F:"Chromium.lnk" /T:"%cd%\chrome-win32\chrome.exe" /P:"--no-first-run --User-data-dir="%cd%\Data" --disk-cache-dir="%USERPROFILE%\ChromeCache""" /W:"%cd%\chrome-win32"
-if exist pepflashplayer.dll Tools\shortcut.exe /A:C /F:"Chromium-PPAPI-FLASH.lnk" /T:"%cd%\chrome-win32\chrome.exe" /P:"--no-first-run --User-data-dir="%cd%\Data" --disk-cache-dir="%USERPROFILE%\ChromeCache" --ppapi-flash-path=%cd%\pepflashplayer.dll --ppapi-flash-version=17.0.0.134"" /W:"%cd%\chrome-win32"
+Tools\shortcut.exe /A:C /F:"Chromium.lnk" /T:"%cd%\chrome-win32\chrome.exe" /P:"--User-data-dir="%cd%\Data" --disk-cache-dir="%USERPROFILE%\ChromeCache""" /W:"%cd%\chrome-win32" /D:"访问互联网"
+if exist pepflashplayer.dll Tools\shortcut.exe /A:C /F:"Chromium-PPAPI-FLASH.lnk" /T:"%cd%\chrome-win32\chrome.exe" /P:"--User-data-dir="%cd%\Data" --disk-cache-dir="%USERPROFILE%\ChromeCache" --ppapi-flash-path=%cd%\pepflashplayer.dll --ppapi-flash-version=17.0.0.134"" /W:"%cd%\chrome-win32" /D:"访问互联网"
+exit
